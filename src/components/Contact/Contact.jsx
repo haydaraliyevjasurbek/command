a3a8c50e3a65a33e3b710be39a80c3407aa00124
Jasurbek Haydaraliyev <129217406+haydaraliyevjasurbek@users.noreply.github.com>
2025-12-17 19:38:06 +0500

    Rename contact.css to Contact.css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Form, Button, Card, Row, Col, Container } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./contact.css";
+import "./Contact.css";
 
 const translations = {
   uz: {

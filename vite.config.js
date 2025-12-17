de36df9406c6b358c0db871b1cb5d6d8fbc25acb
Jasurbek Haydaraliyev <129217406+haydaraliyevjasurbek@users.noreply.github.com>
2025-12-17 19:48:40 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/project-name/", // GitHub Pages uchun kerak, Vercel uchun odatda "/" bo‘lsa yetarli
+  base: "/", // Vercel uchun
 });
